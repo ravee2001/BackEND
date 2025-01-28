8f77430cb6de8e653181a1cd284d7972164c32c3
ravee2001 <s6406021620076@email.kmutnb.ac.th>
2025-01-28 09:43:03 +0700

    done

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.get("/", (req, res) => {
-  res.send("Hello world good");
+  res.send("Hello world!");
 });
 
 app.listen(port, () => {
